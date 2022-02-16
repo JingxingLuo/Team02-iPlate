ed7d88ce2f1412d371ef4206bcbabfb1f905b737
Tunni Chiang <claire890117@gmail.com>
2022-02-16 13:52:05 -0800

    tunni added individual page and remove img folder

--- applications/client/src/App.js
+++ applications/client/src/App.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
+import { Component } from "react";
 import "./App.css";
-=======
-import {Component} from 'react';
-import './App.css';
->>>>>>> 8af401a672191fb2ee9ce9d4484b882bb6df5a09
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
@@ -13,40 +8,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import About from "./components/about.component";
 import PrathibaUser from "./components/prathiba.component";
-<<<<<<< HEAD
-import TunniUser from "./components/tunni.component";
-
-// class App extends React.Component{
-// render() {
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/prathiba" element={<PrathibaUser />} />
-        <Route path="/tunni" element={<TunniUser />} />
-=======
-import ChristopherUser from './components/christopher.component';
+import ChristopherUser from "./components/christopher.component";
 import MTKComponent from "./components/MTK.component";
 
-class App extends Component{
-render() {
-  return (
-    <Router>
-      <br />
-      <Routes>
-      <Route path='/prathiba' element={<PrathibaUser />} />
-      <Route path='/myatkyaw' element={<MTKComponent />} />
-      <Route path="/christopher" element={ChristopherUser} />
-      <Route path='/' exact element={<About />} />
->>>>>>> 8af401a672191fb2ee9ce9d4484b882bb6df5a09
-      </Routes>
-    </Router>
-  );
-<<<<<<< HEAD
-=======
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <br />
+        <Routes>
+          <Route path="/prathiba" element={<PrathibaUser />} />
+          <Route path="/myatkyaw" element={<MTKComponent />} />
+          <Route path="/christopher" element={ChristopherUser} />
+          <Route path="/" exact element={<About />} />
+        </Routes>
+      </Router>
+    );
   }
->>>>>>> 8af401a672191fb2ee9ce9d4484b882bb6df5a09
 }
 
 export default App;
