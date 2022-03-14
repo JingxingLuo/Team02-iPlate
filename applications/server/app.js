8a363de23d93c343a91cc98999649658fc492a4b
JingxingLuo <jluo17@mail.sfsu.edu>
2022-03-13 18:01:47 -0700

    prototype of login and signup

--- applications/server/app.js
+++ applications/server/app.js
@@ -1,11 +1,70 @@
 const express = require('express')
+var mongoose = require('mongoose')
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 const app = express()
-
-app.get('/', (req,res) => {
-    res.send('Hello World!')
-})
+app.use(express.json());
 
 app.listen(8000, () => {
     console.log("TEST express")
+})
+/** 
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("MyDatabase");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("test").insertOne(myobj, function(err, res) {
+     /**  if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }); */
+
+app.get('/', function (req, res) {
+    console.log(req.body)
+    res.send('Hello World!')
+})
+
+/*
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("MyDatabase");
+    var myobj ={name:"Harry",phone:4211};
+    dbo.collection("test").insertOne(myobj, function(err, res) {
+       if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    })
+})*/
+
+var db;
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    dbo = db.db("MyDatabase");
+    dbo.collection("test").findOne({ name: 'Harry' }, function (err, result) {
+        if (err) throw err;
+
+        console.log(result.length);
+        console.log(result.name);
+        console.log(result.phone);
+
+
+    });
+});
+
+
+app.get('/users/login', (req, res, next) => {
+
+    console.log('user login!');
+    dbo.collection("test").findOne({ name: 'Harry' }, function (err, result) {
+
+        if (err) throw err;
+
+
+        console.log(result.name);
+        console.log(result.phone);
+    })
+    res.send('User login triggered');
 })
